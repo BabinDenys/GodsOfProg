74c60511de1ec8316a5f7df65b0b82686cb218a1
Denys Babin <dbabin@klevertech.fun>
2020-04-19 18:45:19 +0300

    first commit

---
+++ scripts/script.js
@@ -0,0 +1,65 @@
+var arr = [
+    {
+        price: 30,
+        disc: 'Это Яблоко',
+        name: "Яблоко"
+    },
+    {
+        price: 304,
+        disc: 'Это пирожок',
+        name: "пирожок"
+    },
+    {
+        price: 90,
+        disc: 'Это помидорка',
+        name: "Помидор"
+    }
+]
+
+
+
+function alert2() {
+    document.createElement('qwer1').appendChild( document.getElementById('container') );
+}
+
+function counterPlus() {
+    console.log(arr)
+    document.getElementById("q1").innerHTML = arr[0].name;
+    document.getElementById("q2").innerHTML = arr[0].disc;
+    document.getElementById("q3").innerHTML = arr[0].price;
+}
+function counterPlus2() {
+    console.log(arr)
+    document.getElementById("q1").innerHTML = arr[1].name;
+    document.getElementById("q2").innerHTML = arr[1].disc;
+    document.getElementById("q3").innerHTML = arr[1].price;
+}
+function counterPlus3() {
+    console.log(arr)
+    document.getElementById("q1").innerHTML = arr[2].name;
+    document.getElementById("q2").innerHTML = arr[2].disc;
+    document.getElementById("q3").innerHTML = arr[2].price;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// 0) числа и строки
+// if and for
+// 1) массивы
+// 2) обьекты
+// document.createElement('block').appendChild( document.createElement('b') );
+
+// массив с данными страниц
+// Каждая ссылка сслыается на конкретные данные
